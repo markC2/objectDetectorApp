979424ac71feeadb9408405989cf512d011cd23a
Mark Carey <marcar2005@hotmail.co.uk>
2025-10-24 16:46:47 +0100

    cameraManager to handle safely using camera and getting output into a previewlayer

--- objectDetectorApp/ContentView.swift
+++ objectDetectorApp/ContentView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var cameraManager = CameraManager()
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+        
         }
         .padding()
+        .onAppear{
+            cameraManager.start()
+        }
+       
     }
+        
+    
 }
 
 #Preview {
