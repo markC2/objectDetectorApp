bd7779ebded555f5f8fc4f8ab1388570b09a7ac5
Mark Carey <marcar2005@hotmail.co.uk>
2025-10-24 17:04:13 +0100

    creating a view for the camera feed to be displayed on, intially a black empty view that gets removed then drawn over as the camera setup is complete to then show the camera feed in realtime

--- objectDetectorApp/ContentView.swift
+++ objectDetectorApp/ContentView.swift
@@ -6,16 +6,15 @@
 //
 
 import SwiftUI
-
+import AVFoundation
 struct ContentView: View {
     @StateObject private var cameraManager = CameraManager()
     var body: some View {
         
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            CameraPreviewView(previewLayer: cameraManager.previewLayer)
+                .ignoresSafeArea(.all)
             
         
         }
@@ -27,6 +26,29 @@ struct ContentView: View {
     }
         
     
+}
+
+struct CameraPreviewView: UIViewRepresentable {
+    let previewLayer: AVCaptureVideoPreviewLayer?
+    
+    func makeUIView(context: Context) -> UIView {
+        let v = UIView()
+        v.backgroundColor = .black
+        return v
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        guard let layer = previewLayer else {
+            
+            return
+        }
+        if layer.superlayer !== uiView.layer {
+            uiView.layer.sublayers?.forEach { $0.removeFromSuperlayer() }
+            uiView.layer.addSublayer(layer)
+            
+        }
+        layer.frame = uiView.bounds
+    }
 }
 
 #Preview {
