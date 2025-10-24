8034d791e500806f510f37dc5d6dbb2850c65f72
Mark Carey <mark.carey@bbc.co.uk>
2025-10-24 15:11:00 +0100

    Initial Commit

---
+++ objectDetectorAppTests/objectDetectorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  objectDetectorAppTests.swift
+//  objectDetectorAppTests
+//
+//  Created by Mark Carey on 24/10/2025.
+//
+
+import Testing
+@testable import objectDetectorApp
+
+struct objectDetectorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
